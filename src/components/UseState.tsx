f6499348d6ccc6eea4d81a108960fe0a0ee2f66d
Yeison Silva <yeisonsilvat06@gmail.com>
2024-07-30 23:02:50 -0500

    Maquetando el comp UseState

--- src/components/UseState.tsx
+++ src/components/UseState.tsx
@@ -1,8 +1,23 @@
-import { useState } from "react"
+import { useState,useEffect } from "react"
 
 
 function UseState({name}: {name: string}) {
     const [error, setError] = useState(false)
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        console.log("Empezando el efecto")
+
+        if(loading) {
+            setTimeout(() => {
+                console.log("haciendo la validación")
+
+                setLoading(false)
+
+                console.log("Terminando la validación")
+            }, 3000)
+        }
+    }, [loading])
 
     return (
         <div>
@@ -12,10 +27,15 @@ function UseState({name}: {name: string}) {
 
             {error && (
                 <p>Error: el codigo es incorrecto</p>
-            )}            
+            )}
+            {
+                loading && (
+                    <p>Cargando...</p>
+                )
+            }
             <input placeholder="Codigo de seguridad" />
             <button
-                onClick={() => setError(!error)}
+                onClick={() => setLoading(true)}
             >Comprobar</button>
         </div>
 
